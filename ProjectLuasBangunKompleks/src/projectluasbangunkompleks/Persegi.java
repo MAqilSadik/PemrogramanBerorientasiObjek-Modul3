17f8ac62ad69130587f7d43de0d9139c4aacb31d
Aqil Sadik <dmadaaqil@gmail.com>
2021-10-10 21:44:02 +0700

    Create Persegi.java

---
+++ ProjectLuasBangunKompleks/src/projectluasbangunkompleks/Persegi.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectluasbangunkompleks;
+
+/**
+ *
+ * @author User
+ */
+public class Persegi {
+    int s;
+
+    int hitungLuas(int s){
+        int luas = s * s;
+        return luas;
+    }
+}
